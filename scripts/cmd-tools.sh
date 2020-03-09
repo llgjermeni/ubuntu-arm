476e2cd2bb5c2769f3749a0bbdece44d59727b3a
llazar <laertgjermeni@hotmail.com>
2020-03-09 10:25:02 +0100

    changes

--- scripts/cmd-tools.sh
+++ scripts/cmd-tools.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -x
+trap read debug
 
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+
 sudo apt-get update 
-sudo apt-get install mssql-tools unixodbc-dev
+
+sudo apt-get --yes --force-yes install mssql-tools unixodbc-dev
+
+
